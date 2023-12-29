7ea520f9653eb813f5b59a1ac1c774ef0db57943
Rafa <jr.vera.ma@gmail.com>
2023-12-29 22:37:14 +0100

    final adjustment Spark consumes from Socket

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,19 +1,30 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark import SparkContext, SparkConf
-
-# Configurar la ubicación de WINUTILS.EXE
-conf = SparkConf().set("spark.hadoop.winutils.path", "Ruta\\al\\directorio\\del\\proyecto\\con\\WINUTILS.EXE")
-
-# Crear una sesión Spark
-sc = SparkContext(appName="MiAplicacionSpark", conf=conf)
+from pyspark.sql.types import StructField, StringType, StructType, FloatType
+from pyspark.sql.functions import from_json, col, when, udf
 
 
 def start_streaming(spark):
-    stream_df = spark.readStream.format("socket").option("host", "localhost").option("port", 9999).load()
+    try:
 
-    query = stream_df.writeStream.outputMode("append").format("console").start()
-    query.awaitTermination()
+        stream_df = spark.readStream.format("socket").option("host", "localhost").option("port", 9999).load()
+
+        schema = StructType([
+            StructField("review_id", StringType()),
+            StructField("user_id", StringType()),
+            StructField("business_id", StringType()),
+            StructField("stars", FloatType()),
+            StructField("date", StringType()),
+            StructField("text", StringType())
+        ])
+
+        stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select("data.*")
+
+        query = stream_df.writeStream.outputMode("append").format("console").options(truncate=False).start()
+        query.awaitTermination()
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
