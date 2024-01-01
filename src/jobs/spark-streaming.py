072cb34f08ff7c651c302270ee3a01ee383e7966
Rafa <jr.vera.ma@gmail.com>
2024-01-01 21:27:03 +0100

    configured kafka settings and Concluent.cloud

--- src/jobs/spark-streaming.py
+++ src/jobs/spark-streaming.py
@@ -1,10 +1,13 @@
+from time import sleep
+
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StringType, StructType, FloatType
 from pyspark.sql.functions import from_json, col, when, udf
-
+from config.config import config
 
 def start_streaming(spark):
+    topic = 'customers_review'
     try:
 
         stream_df = spark.readStream.format("socket").option("host", "localhost").option("port", 9999).load()
@@ -20,11 +23,26 @@ def start_streaming(spark):
 
         stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select("data.*")
 
-        query = stream_df.writeStream.outputMode("append").format("console").options(truncate=False).start()
-        query.awaitTermination()
+        kafka_df = stream_df.selectExpr(("CAST(review_id AS STRING) AS key", "to_json(struct(*)) AS value"))
+
+        query = (kafka_df.writeStream
+               .format("kafka")
+               .option("kafka.bootstrap.servers", config['kafka']['bootstrap.servers'])
+               .option("kakfa.security.protocol", config['kafka']['security.protocol'])
+               .option('kafka.sasl.mechanism', config['kafka']['sasl.mechanism'])
+               .option('kafka.sasl.jaas.config', 'org.apache.kafka.common.security.plain.PlainLoginModule required username="{username}" '
+                       'password="{password}";'.format(
+                            username=config['kafka']['sasl.username'],
+                            pasword=config['kafka']['sasl.password']
+                        ))
+               .option('checkpointLocation', '/tmp/checkpoint')
+               .option('topic', topic)
+               .awaitTermination()
+               )
 
     except Exception as e:
-        print(e)
+        print(f'Exception encountered: {e}. Retrying in 10 seconds')
+        sleep(10)
 
 
 if __name__ == "__main__":
